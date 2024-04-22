03b99e1e717b19791faef4de0fd7ca103eb41f72
Brandon Willetts”git config --global user.email “brandonwilletts@gmail.com”git config --global user.name Brandon <brandonwilletts@gmail.com>
2024-04-22 14:42:27 -0700

    Add and style "Remove" "Read/Unread" buttons

--- javascript.js
+++ javascript.js
@@ -18,11 +18,14 @@ buttonCancel.addEventListener("click", () => {
 
 buttonSave.addEventListener("click", (event) => {
     event.preventDefault();
+    
     const book = new Book(title.value, author.value, pages.value);
         read.checked ? book.read = "Read" : book.read = "Not Read";
     addBookToLibrary(book);
+    
     const bookAsArray = [book];
     displayBooksInLibrary(bookAsArray);
+    
     dialog.close();
 });
 
@@ -43,10 +46,21 @@ function addBookToLibrary(book) {
 function displayBooksInLibrary(books) {
     for (let i = 0; i < books.length; i++) {
         const tableRow = document.createElement("tr");
+        
         const title = document.createElement("td");
         const author = document.createElement("td");
         const pages = document.createElement("td");
         const read = document.createElement("td");
+
+        const action = document.createElement("td");
+        const buttonDiv = document.createElement("div");
+            buttonDiv.style.cssText = "display: flex; gap: 8px; flex-wrap: wrap;";
+        const removeButton = document.createElement("button");
+            removeButton.classList = "btn-red btn-sm";
+            removeButton.textContent = "Remove";
+        const toggleReadButton = document.createElement("button");
+            toggleReadButton.classList = "btn-green btn-sm";
+            books[i].read === true ? toggleReadButton.textContent = "Mark as Unread" : toggleReadButton.textContent = "Mark as Read";
 
         title.textContent = books[i].title;
         author.textContent = books[i].author;
@@ -54,23 +68,29 @@ function displayBooksInLibrary(books) {
         read.textContent = books[i].read;
 
         libraryTableBody.appendChild(tableRow);
+
         tableRow.appendChild(title);
         tableRow.appendChild(author);
         tableRow.appendChild(pages);
         tableRow.appendChild(read);
+
+        tableRow.appendChild(action);
+        action.appendChild(buttonDiv);
+        buttonDiv.appendChild(removeButton);
+        buttonDiv.appendChild(toggleReadButton);
     }
 }
 
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet");
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, true);
 addBookToLibrary(theHobbit);
 
-const theGreatGatsby = new Book("The Great Gatsby", "Scott Fitzgerald", 395, "not read yet");
+const theGreatGatsby = new Book("The Great Gatsby", "Scott Fitzgerald", 395, false);
 addBookToLibrary(theGreatGatsby);
 
-const warAndPeace = new Book("War and Peace", "Leo Tolstoy", 1652, "not read yet");
+const warAndPeace = new Book("War and Peace", "Leo Tolstoy", 1652, true);
 addBookToLibrary(warAndPeace);
 
-const wiseguy = new Book("Wiseguy", "Nicolas Pileggi", 424, "read");
+const wiseguy = new Book("Wiseguy", "Nicolas Pileggi", 424, false);
 addBookToLibrary(wiseguy);
 
 displayBooksInLibrary(myLibrary);
