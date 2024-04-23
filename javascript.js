992ae6f0ab118e51df5fd49657e8a412efb546ce
Brandon Willetts”git config --global user.email “brandonwilletts@gmail.com”git config --global user.name Brandon <brandonwilletts@gmail.com>
2024-04-22 20:00:10 -0700

    Refactor display book in tbl and remove from tbl

--- javascript.js
+++ javascript.js
@@ -28,78 +28,79 @@ buttonCancel.addEventListener("click", () => {
 form.addEventListener("submit", (event) => {
     event.preventDefault();
     const book = new Book(title.value, author.value, pages.value);
-    read.checked ? book.read = "Read" : book.read = "Not Read";
-    addBookToLibrary(book);
+        read.checked ? book.read = "Read" : book.read = "Not Read";
+    addBookToMyLibrary(book);
     dialog.close();
 });
 
-function addBookToLibrary(book) {
+function addBookToMyLibrary(book) {
     myLibrary.push(book);
+    displayBookInTable(book);
 }
 
-function removeBookFromLibrary(book) {
+function removeBookFromMyLibrary(book) {
     myLibrary.splice(book.indexOf(), 1);
-    //const rowToRemove = document.querySelector(`tr[index='${index}']`);
-    //rowToRemove.remove();
+    removeBookFromTable(book);
 }
 
-// function displayBooksInLibrary(books) {
-//     for (let i = 0; i < books.length; i++) {
-//         const tableRow = document.createElement("tr");
-//         const title = document.createElement("td");
-//         const author = document.createElement("td");
-//         const pages = document.createElement("td");
-//         const read = document.createElement("td");
+function displayBookInTable(book) {
+    const tableRow = document.createElement("tr");
+    const title = document.createElement("td");
+        title.textContent = book.title;
+    const author = document.createElement("td");
+        author.textContent = book.author;
+    const pages = document.createElement("td");
+        pages.textContent = book.pages;
+    const read = document.createElement("td");
+        read.textContent = book.read;
+    const action = document.createElement("td");
+    const buttonDiv = document.createElement("div");
+        buttonDiv.style.cssText = "display: flex; gap: 8px; flex-wrap: wrap;";
+    const removeButton = document.createElement("button");
+        removeButton.classList = "btn-red btn-sm btn-remove";
+        removeButton.textContent = "Remove";
+    const toggleReadButton = document.createElement("button");
+        if ( book.read === true) {
+            toggleReadButton.textContent = "Mark as Unread"
+            toggleReadButton.classList = "btn-yellow btn-sm btn-toggle";
+        } else {
+            toggleReadButton.textContent = "Mark as Read"
+            toggleReadButton.classList = "btn-green btn-sm btn-toggle";
+        }
+        
+    libraryTable.appendChild(tableRow);
+        tableRow.appendChild(title);
+        tableRow.appendChild(author);
+        tableRow.appendChild(pages);
+        tableRow.appendChild(read);
+        tableRow.appendChild(action);
+            action.appendChild(buttonDiv);
+                buttonDiv.appendChild(removeButton);
+                buttonDiv.appendChild(toggleReadButton);
+}
 
-//         const action = document.createElement("td");
-//         const buttonDiv = document.createElement("div");
-//             buttonDiv.style.cssText = "display: flex; gap: 8px; flex-wrap: wrap;";
-//         const removeButton = document.createElement("button");
-//             removeButton.classList = "btn-red btn-sm btn-remove";
-//             removeButton.textContent = "Remove";
-//         const toggleReadButton = document.createElement("button");
-//             if ( books[i].read === true) {
-//                 toggleReadButton.textContent = "Mark as Unread"
-//                 toggleReadButton.classList = "btn-yellow btn-sm btn-toggle";
-//             } else {
-//                 toggleReadButton.textContent = "Mark as Read"
-//                 toggleReadButton.classList = "btn-green btn-sm btn-toggle";
-//             }
+function removeBookFromTable(book) {
+    const rowToRemove = document.querySelector(`tr[index='${book.indexOf()}']`);
+    rowToRemove.remove();
+}
 
-//         title.textContent = books[i].title;
-//         author.textContent = books[i].author;
-//         pages.textContent = books[i].pages;
-//         read.textContent = books[i].read;
-
-//         libraryTable.appendChild(tableRow);
-//         tableRow.appendChild(title);
-//         tableRow.appendChild(author);
-//         tableRow.appendChild(pages);
-//         tableRow.appendChild(read);
-
-//         tableRow.appendChild(action);
-//         action.appendChild(buttonDiv);
-//         buttonDiv.appendChild(removeButton);
-//         buttonDiv.appendChild(toggleReadButton);
-//     }
-//     // const buttonsRemove = document.querySelectorAll(".btn-remove");
-//     // buttonsRemove.forEach((button) => {
-//     //     button.addEventListener("click", () => {
-//     //         removeBookFromLibrary(button.getAttribute("index"));
-//     //     })
-//     // })
-// }
+//     const buttonsRemove = document.querySelectorAll(".btn-remove");
+//     buttonsRemove.forEach((button) => {
+//     button.addEventListener("click", () => {
+//     removeBookFromMyLibrary(button.getAttribute("index"));
+//     })
+//     })
 
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, true);
-addBookToLibrary(theHobbit);
+addBookToMyLibrary(theHobbit);
 
 const theGreatGatsby = new Book("The Great Gatsby", "Scott Fitzgerald", 395, false);
-addBookToLibrary(theGreatGatsby);
+addBookToMyLibrary(theGreatGatsby);
 
 const warAndPeace = new Book("War and Peace", "Leo Tolstoy", 1652, true);
-addBookToLibrary(warAndPeace);
+addBookToMyLibrary(warAndPeace);
 
 const wiseguy = new Book("Wiseguy", "Nicolas Pileggi", 424, false);
-addBookToLibrary(wiseguy);
+addBookToMyLibrary(wiseguy);
 
 //displayBooksInLibrary(myLibrary);
