e9b763109549645ab6de33d38160d28d9baffcc3
Brandon Willetts”git config --global user.email “brandonwilletts@gmail.com”git config --global user.name Brandon <brandonwilletts@gmail.com>
2024-04-22 19:44:07 -0700

    Fix "submit" button
    Change event listener to "form", not the "button".
    Clicking the save button now adds the book to the array.

--- javascript.js
+++ javascript.js
@@ -1,33 +1,10 @@
 const myLibrary = [];
 
-const libraryTableBody = document.querySelector("#library");
+const libraryTable = document.querySelector("#library");
 const dialog = document.querySelector("dialog");
-const form = document.querySelector("form");
 const buttonNewBook = document.querySelector("#btn-new-book");
 const buttonCancel = document.querySelector("#cancel");
-const buttonSave = document.querySelector("#save");
-
-buttonNewBook.addEventListener("click", () => {
-    form.reset();
-    dialog.showModal();
-});
-
-buttonCancel.addEventListener("click", () => {
-    dialog.close();
-});
-
-buttonSave.addEventListener("click", (event) => {
-    event.preventDefault();
-    
-    const book = new Book(title.value, author.value, pages.value);
-        read.checked ? book.read = "Read" : book.read = "Not Read";
-    addBookToLibrary(book);
-    
-    const bookAsArray = [book];
-    displayBooksInLibrary(bookAsArray);
-    
-    dialog.close();
-});
+const form = document.querySelector("form");
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -39,47 +16,79 @@ function Book(title, author, pages, read) {
     }
 }
 
+buttonNewBook.addEventListener("click", () => {
+    form.reset();
+    dialog.showModal();
+});
+
+buttonCancel.addEventListener("click", () => {
+    dialog.close();
+});
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const book = new Book(title.value, author.value, pages.value);
+    read.checked ? book.read = "Read" : book.read = "Not Read";
+    addBookToLibrary(book);
+    dialog.close();
+});
+
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
-function displayBooksInLibrary(books) {
-    for (let i = 0; i < books.length; i++) {
-        const tableRow = document.createElement("tr");
-        
-        const title = document.createElement("td");
-        const author = document.createElement("td");
-        const pages = document.createElement("td");
-        const read = document.createElement("td");
-
-        const action = document.createElement("td");
-        const buttonDiv = document.createElement("div");
-            buttonDiv.style.cssText = "display: flex; gap: 8px; flex-wrap: wrap;";
-        const removeButton = document.createElement("button");
-            removeButton.classList = "btn-red btn-sm";
-            removeButton.textContent = "Remove";
-        const toggleReadButton = document.createElement("button");
-            toggleReadButton.classList = "btn-green btn-sm";
-            books[i].read === true ? toggleReadButton.textContent = "Mark as Unread" : toggleReadButton.textContent = "Mark as Read";
-
-        title.textContent = books[i].title;
-        author.textContent = books[i].author;
-        pages.textContent = books[i].pages;
-        read.textContent = books[i].read;
-
-        libraryTableBody.appendChild(tableRow);
-
-        tableRow.appendChild(title);
-        tableRow.appendChild(author);
-        tableRow.appendChild(pages);
-        tableRow.appendChild(read);
-
-        tableRow.appendChild(action);
-        action.appendChild(buttonDiv);
-        buttonDiv.appendChild(removeButton);
-        buttonDiv.appendChild(toggleReadButton);
-    }
+function removeBookFromLibrary(book) {
+    myLibrary.splice(book.indexOf(), 1);
+    //const rowToRemove = document.querySelector(`tr[index='${index}']`);
+    //rowToRemove.remove();
 }
+
+// function displayBooksInLibrary(books) {
+//     for (let i = 0; i < books.length; i++) {
+//         const tableRow = document.createElement("tr");
+//         const title = document.createElement("td");
+//         const author = document.createElement("td");
+//         const pages = document.createElement("td");
+//         const read = document.createElement("td");
+
+//         const action = document.createElement("td");
+//         const buttonDiv = document.createElement("div");
+//             buttonDiv.style.cssText = "display: flex; gap: 8px; flex-wrap: wrap;";
+//         const removeButton = document.createElement("button");
+//             removeButton.classList = "btn-red btn-sm btn-remove";
+//             removeButton.textContent = "Remove";
+//         const toggleReadButton = document.createElement("button");
+//             if ( books[i].read === true) {
+//                 toggleReadButton.textContent = "Mark as Unread"
+//                 toggleReadButton.classList = "btn-yellow btn-sm btn-toggle";
+//             } else {
+//                 toggleReadButton.textContent = "Mark as Read"
+//                 toggleReadButton.classList = "btn-green btn-sm btn-toggle";
+//             }
+
+//         title.textContent = books[i].title;
+//         author.textContent = books[i].author;
+//         pages.textContent = books[i].pages;
+//         read.textContent = books[i].read;
+
+//         libraryTable.appendChild(tableRow);
+//         tableRow.appendChild(title);
+//         tableRow.appendChild(author);
+//         tableRow.appendChild(pages);
+//         tableRow.appendChild(read);
+
+//         tableRow.appendChild(action);
+//         action.appendChild(buttonDiv);
+//         buttonDiv.appendChild(removeButton);
+//         buttonDiv.appendChild(toggleReadButton);
+//     }
+//     // const buttonsRemove = document.querySelectorAll(".btn-remove");
+//     // buttonsRemove.forEach((button) => {
+//     //     button.addEventListener("click", () => {
+//     //         removeBookFromLibrary(button.getAttribute("index"));
+//     //     })
+//     // })
+// }
 
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, true);
 addBookToLibrary(theHobbit);
@@ -93,4 +102,4 @@ addBookToLibrary(warAndPeace);
 const wiseguy = new Book("Wiseguy", "Nicolas Pileggi", 424, false);
 addBookToLibrary(wiseguy);
 
-displayBooksInLibrary(myLibrary);
+//displayBooksInLibrary(myLibrary);
